bca75c6324e546f4b1ec5e45267f18ba5ca24fc4
Paul Milla <paul.milla1@utexas.edu>
2015-01-04 23:32:05 -0600

    Duplicated RequirementsPage View functionality as a fragment inside MainActivity

--- app/src/main/java/com/house_panini/paulm/apo_app/MainActivity.java
+++ app/src/main/java/com/house_panini/paulm/apo_app/MainActivity.java
@@ -9,6 +9,15 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Iterator;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -19,7 +28,7 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new RequirementsFragment())
                     .commit();
         }
     }
@@ -60,15 +69,52 @@ public class MainActivity extends ActionBarActivity {
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
+    public static class RequirementsFragment extends Fragment {
 
-        public PlaceholderFragment() {
+        public RequirementsFragment() {
         }
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_requirements, container, false);
+
+            JSONObject json = ApoOnline.getRequirements();
+            LinearLayout myLayout = (LinearLayout) rootView.findViewById(R.id.requirements_layout);
+
+            Iterator<String> iterator = json.keys();
+            while (iterator.hasNext()) {
+                final String nextTitle = iterator.next();
+                View card = View.inflate(getActivity(), R.layout.requirement_card, null);
+
+                TextView title_text = (TextView) card.findViewById(R.id.title_text);
+                title_text.setText(nextTitle);
+
+                //BUG: Rotating device displays incorrectly displays the progress bar
+                ProgressBar progressBar = (ProgressBar) card.findViewById(R.id.progress_bar);
+                try {
+                    JSONObject value = json.getJSONObject(nextTitle);
+                    int max = value.getInt("max");
+                    int progress = value.getInt("progress");
+                    progressBar.setMax(max);
+                    progressBar.setProgress(progress);
+                    //TODO: Display "progress/max" as fraction or "progress of max"
+                } catch (JSONException e) {
+//                throw new RuntimeException(e);
+                }
+
+                card.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        //TODO: Something when clicked
+                        String text = nextTitle+" pressed!";
+                        Toast.makeText(getActivity(), text, Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                myLayout.addView(card);
+            }
+
             return rootView;
         }
     }
