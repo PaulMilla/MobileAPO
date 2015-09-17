51acf3865210799c956346abfa57e4a6750c7b22
Paul Milla <paul.milla1@utexas.edu>
2015-09-16 22:49:03 -0500

    Fixed basic configs in Android Studio when importing project

--- app/src/main/java/com/house_panini/paulm/apo_app/NavigationDrawerFragment.java
+++ app/src/main/java/com/house_panini/paulm/apo_app/NavigationDrawerFragment.java
@@ -254,10 +254,10 @@ public class NavigationDrawerFragment extends Fragment {
             return true;
         }
 
-        if (item.getItemId() == R.id.action_example) {
-            Toast.makeText(getActivity(), "Example action.", Toast.LENGTH_SHORT).show();
-            return true;
-        }
+//        if (item.getItemId() == R.id.action_example) {
+//            Toast.makeText(getActivity(), "Example action.", Toast.LENGTH_SHORT).show();
+//            return true;
+//        }
 
         return super.onOptionsItemSelected(item);
     }
@@ -280,7 +280,7 @@ public class NavigationDrawerFragment extends Fragment {
     /**
      * Callbacks interface that all activities using this fragment must implement.
      */
-    public static interface NavigationDrawerCallbacks {
+    public interface NavigationDrawerCallbacks {
         /**
          * Called when an item in the navigation drawer is selected.
          */
